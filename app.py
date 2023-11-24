88ac81ef05f8e2c8e0f7f98fc62582e3571d992a
Sahil <sahilkhane410@gmail.com>
2023-11-24 15:25:21 +0530

    first weather app commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask , request , render_template
+
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def displayWebPage():
+    return render_template('index.html')
+
+@app.route('/weatherroute',methods = ['POST'])
+def fetchdata():
+    city = request.form.get('city')
+    unit = request.form.get('unit')
+    appid = request.form.get('appid')
+
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+
+    params = {'q':city,'unit':unit,'appid':appid}
+
+    datas = requests.get(url,params=params)
+
+    data = datas.json() 
+
+    return f'city  = {city}  , unit  = {unit} , appid  = {appid} \n\n {data}'
+
+if __name__ == "__main__" :
+    app.run(host="0.0.0.0")
+
